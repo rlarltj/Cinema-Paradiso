872e78ecfcdc5e59c316811ca2cbab859a0d0e1c
김기서 <5rne@naver.com>
2022-11-28 02:30:15 +0900

    feat: customer 컨트롤러와 등록 폼 dto 생성

---
+++ src/main/java/com/prgrms/movieprj/controller/CustomerController.java
@@ -0,0 +1,34 @@
+package com.prgrms.movieprj.controller;
+
+import com.prgrms.movieprj.domain.Customer;
+import com.prgrms.movieprj.dto.CustomerRegisterForm;
+import com.prgrms.movieprj.dto.ReviewDto;
+import com.prgrms.movieprj.service.CustomerService;
+import com.prgrms.movieprj.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/customer")
+public class CustomerController {
+    private final CustomerService customerService;
+    private final ReviewService reviewService;
+
+    @GetMapping("/{customerId}/review")
+    public ResponseEntity<List<ReviewDto>> showCustomerReviews(@PathVariable int customerId) {
+        List<ReviewDto> reviews = reviewService.findByCustomer(customerId);
+
+        return new ResponseEntity<>(reviews, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Customer> register(@RequestBody CustomerRegisterForm registerForm) {
+        Customer customer = customerService.join(registerForm);
+        return new ResponseEntity<>(customer, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/prgrms/movieprj/dto/CustomerRegisterForm.java
@@ -0,0 +1,10 @@
+package com.prgrms.movieprj.dto;
+
+import lombok.Data;
+
+@Data
+public class CustomerRegisterForm {
+    private String email;
+    private String name;
+    private String phoneNumber;
+}
